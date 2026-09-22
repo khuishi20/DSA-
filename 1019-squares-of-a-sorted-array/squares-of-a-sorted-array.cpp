c25adc0d3f7739a322b41937424ee67243ffc0ef
Khushi Sharma <khusharma2005@gmail.com>
2026-09-22 10:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (5.20%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,62 +1,60 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        vector <int> pos;
+        vector <int>neg;
         int siz = nums.size();
-        vector<int>pos;
-        vector<int>neg;
-for(int i =0;i<siz;i++){
-        if(nums[i]<0){
-            neg.push_back(nums[i]);
+        for(int i=0;i<siz;i++){
+            if(nums[i]<0){
+                neg.push_back(nums[i]);
+            }
+            else{
+                pos.push_back(nums[i]);
+            }
         }
-        else{
-            pos.push_back(nums[i]);
+        int i=0;
+        int j =0;
+        int id=0;
+        int n = neg.size();
+        int m = pos.size();
+        vector<int>arr(n+m);
+        for(int i =0;i<n;i++){
+            neg[i]=neg[i]*neg[i];
+
         }
-}
+        reverse(neg.begin(),neg.end());
+        for(int j=0;j<m;j++){
+            pos[j]=pos[j]*pos[j];
+        }
+
+        while(i<n && j<m){
+            if(neg[i]<pos[j]){
+                arr[id]=neg[i];
+                i++;
+                id++;
+            }
+            else{
+                arr[id]=pos[j];
+                j++;
+                id++;
+            }
+
+        }
+        while(i<n){
+            arr[id]=neg[i];
+                i++;
+                id++;
+
+        }
+        while(j<m){
+            arr[id]=pos[j];
+                j++;
+                id++;
+  
+        }
+        return arr;
 
 
-int i=0;
-int j=0;
-int id = 0;
-int n = pos.size();
-int m = neg.size();
-vector<int>arr(n+m);
-for(int i =0;i<m;i++){
-    neg[i]=neg[i]*neg[i];
-    
-}
-reverse(neg.begin(),neg.end());
-for(int i =0;i<n;i++){
-    pos[i]=pos[i]*pos[i];
-}
-while(i<m && j<n){
-    if(neg[i]<pos[j]){
-    arr[id]=neg[i];
-    i++;
-    id++;
+        
     }
-    else{
-        arr[id]=pos[j];
-    j++;
-    id++;
-
-    }
-
-}
-while(i<m){
-    arr[id]=neg[i];
-    i++;
-    id++;
-
-}
-while(j<n){
-    arr[id]=pos[j];
-    j++;
-    id++;
-}
-return arr;
-
-
-
-
-}
 };
